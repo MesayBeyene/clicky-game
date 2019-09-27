6725c19fa7c24568cf43d77ad533d61177dfbeb3
MesayBeyene <mesibella@gmail.com>
2019-09-27 10:57:52 -0700

    final commit

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -3,19 +3,44 @@ import Navbar from "./Navbar.js";
 import Header from "./Header.js";
 import Main from "./Main.js";
 import "./App.css";
+import ImgOne from "../src/image/imgOne.jpg";
+import ImgTwo from "../src/image/imgTwo.jpg";
+import ImgThree from "../src/image/imgThree.jpg";
+import ImgFour from "../src/image/imgFour.jpg";
+import ImgFive from "../src/image/imgFive.jpg";
+import ImgSix from "../src/image/imgSix.jpg";
+import ImgSeven from "../src/image/imgSeven.jpg";
+import ImgEight from "../src/image/imgEight.jpg";
+import ImgNine from "../src/image/imgNine.jpg";
+import ImgTen from "../src/image/imgTen.jpg";
+import ImgEleven from "../src/image/imgEleven.jpg";
+import ImgTwelve from "../src/image/imgTwelve.jpg";
+import ImgThirteen from "../src/image/imgThirteen.jpg";
+import ImgFourteen from "../src/image/imgFourteen.jpg";
+import ImgFifteen from "../src/image/imgFifteen.jpg";
+import ImgSixteen from "../src/image/imgSixteen.jpg";
 
 class App extends React.Component {
   state = {
     cards: [
-      "dog",
-      "cat",
-      "apple",
-      "orange",
-      "carrot",
-      "Banana",
-      "sleppycat",
-      "dogSleepy"
+      <img alt="imgone" src={ImgOne} />,
+      <img alt="imgtwo" src={ImgTwo} />,
+      <img alt="imgthree" src={ImgThree} />,
+      <img alt="imgfour" src={ImgFour} />,
+      <img alt="imgfive" src={ImgFive} />,
+      <img alt="imgsix" src={ImgSix} />,
+      <img alt="imgseven" src={ImgSeven} />,
+      <img alt="imgeight" src={ImgEight} />,
+      <img alt="imgnine" src={ImgNine} />,
+      <img alt="imgten" src={ImgTen} />,
+      <img alt="imgeleven" src={ImgEleven} />,
+      <img alt="imgtwelve" src={ImgTwelve} />,
+      <img alt="imgthirteen" src={ImgThirteen} />,
+      <img alt="imgfourteen" src={ImgFourteen} />,
+      <img alt="imgfifeteen" src={ImgFifteen} />,
+      <img alt="imgsixisteen" src={ImgSixteen} />
     ],
+
     score: 0,
     clickedCards: [],
     topScore: 0
--- clicky-game/src/Header.js
+++ clicky-game/src/Header.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./Header.css";
 function Header() {
   return (
-    <div className="header">
-      <h1>Clicky Game!</h1>
-      <h2>
-        Click on an image to earn points, but don't click on any more than once!
-      </h2>
+    <div className="wrapper">
+      <div className="header">
+        <h1>Clicky Game!</h1>
+        <h2>
+          Click on an image to earn points, but don't click on any more than
+          once!
+        </h2>
+      </div>
     </div>
   );
 }
